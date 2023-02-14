9d36f45c572eeb8706170cb907a4d9be2637553f
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2023-02-14 14:19:41 +0530

    Supabase React Basic Crud  App

---
+++ src/createClient.js
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+export const supabase = createClient(
+	'https://xiegrllsuqlivaubyria.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZWdybGxzdXFsaXZhdWJ5cmlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYzNjIxNzcsImV4cCI6MTk5MTkzODE3N30.lCP27kOdWzcwQZh7QE2bzgqTY9h43fRLzGzzuLe5X6s'
+);
+ 
+	// github thilaks
+// export const supabase= createClient(
+//     "https://tupmvvksqgbqdzridrqb.supabase.co",
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cG12dmtzcWdicWR6cmlkcnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcwMTIyNTcsImV4cCI6MTk4MjU4ODI1N30.5uZdCofPa878ZbK6jbrl6pJBLmn11XC9BAausn2tD2U"
+//     )
